5227b2c08f7abc52530c71e1b878ba7a6d25e4c0
Yann Zaoui <yzaoui7@gmail.com>
2019-11-08 18:04:53 -0500

    Create basic reply bot

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("../config.json");
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+});
+
+client.on("message", message => {
+    if (message.content === "ping") {
+        message.channel.send("pong");
+    }
+});
+
+client.login(config.token);
