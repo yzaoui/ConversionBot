aa650eed4e6e860412aea5265c70749e062b5419
Yann Zaoui <yzaoui7@gmail.com>
2019-11-08 18:50:11 -0500

    Implement regex to capture temperatures in chat

--- src/index.js
+++ src/index.js
@@ -7,9 +7,33 @@ client.on("ready", () => {
 });
 
 client.on("message", message => {
-    if (message.content === "ping") {
-        message.channel.send("pong");
+    if (message.author.bot) return;
+
+    const results = message.content.matchAll(/\b(?<degrees>\d+)[ °]?(?<unit>[FC])\b/gmi);
+
+    const outputs = [];
+
+    for (let result of results) {
+        let { degrees, unit } = result.groups;
+        unit = unit.toUpperCase();
+
+        let degreesConv;
+        let unitConv;
+
+        if (unit === "F") {
+            degreesConv = (degrees - 32) * 5 / 9;
+            unitConv = "C";
+        } else {
+            degreesConv = (degrees * 9 / 5) + 32;
+            unitConv = "F";
+        }
+
+        degreesConv = degreesConv.toFixed(1);
+
+        outputs.push(`${degrees}°${unit} = ${degreesConv}°${unitConv}`);
     }
+
+    if (outputs.length > 0) message.channel.send(outputs.join("\n"));
 });
 
 client.login(config.token);
